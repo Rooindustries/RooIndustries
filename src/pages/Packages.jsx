ca615bdbb2d0eb697cbf49a8f0a8287940917e0d
LoaiMaher <loai.m.albadawi@gmail.com>
2025-12-06 07:20:10 +0300

    Footer on packages

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -1,5 +1,10 @@
 import Footer from "../components/Footer";
 import PackagesComp from "../components/Packages";
 export default function Packages() {
-  return <>{<PackagesComp />}</>;
+  return (
+    <>
+      <PackagesComp />
+      <Footer />
+    </>
+  );
 }
