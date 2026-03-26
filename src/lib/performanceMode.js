8ab43555ea98a6b74231f638763267354a04edf8
servi roo <serviroo@servis-M5-Max-Macbook-Pro.local>
2026-03-26 13:57:36 +0530

    refactor: make lite mode permanent

--- src/lib/performanceMode.js
+++ src/lib/performanceMode.js
@@ -1,26 +1,10 @@
 import { useSyncExternalStore } from "react";
 
-const PERFORMANCE_MODE_EVENT = "roo-performance-mode-change";
-const LOW_PERFORMANCE_CLASS = "low-performance-mode";
+const subscribeNoop = () => () => {};
 
-export const isLowPerformanceModeEnabled = () => {
-  if (typeof document === "undefined") return false;
-  return document.documentElement.classList.contains(LOW_PERFORMANCE_CLASS);
-};
+export const isLowPerformanceModeEnabled = () => true;
 
-export const subscribePerformanceMode = (listener) => {
-  if (typeof window === "undefined") {
-    return () => {};
-  }
-
-  const handleChange = () => listener();
-  window.addEventListener(PERFORMANCE_MODE_EVENT, handleChange);
-  return () => window.removeEventListener(PERFORMANCE_MODE_EVENT, handleChange);
-};
+export const subscribePerformanceMode = (_listener) => subscribeNoop();
 
 export const useLowPerformanceMode = () =>
-  useSyncExternalStore(
-    subscribePerformanceMode,
-    isLowPerformanceModeEnabled,
-    () => false
-  );
+  useSyncExternalStore(subscribeNoop, () => true, () => true);
