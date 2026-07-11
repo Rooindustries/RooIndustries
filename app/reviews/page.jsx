668db0e69682f5796e60a8ad0714be0ac8f8bc01
serviroo <serviroo@rooindustries.com>
2026-07-11 23:54:23 +0530

    perf: serve proof pages from the edge

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -2,7 +2,8 @@ import RouteRenderer from "@/src/next/RouteRenderer";
 import seo from "@/src/lib/seo";
 
 export const metadata = seo.getMetadataForPath("/reviews");
+export const dynamic = "force-static";
 
-export default function Page({ searchParams }) {
-  return <RouteRenderer pathname="/reviews" searchParams={searchParams} />;
+export default function Page() {
+  return <RouteRenderer pathname="/reviews" searchParams={{}} />;
 }
